6727f1e37b8dfad0fcbbcb1b255dc8d550d42413
AngelOso20 <angeloso20@users.noreply.github.com>
2025-11-11 20:35:10 -0500

    ajuste padding en cajas moviles

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: false,
+    remotePatterns: [],
+  },
+  // Configuración para evitar problemas de conexión
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
 }
 
 module.exports = nextConfig
